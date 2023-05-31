7413a0c66a1b7285cbfc2191e4ab01f520949127
Sofia Chernikova <sofiachern2003@yandex.ru>
2023-05-31 16:55:08 +0300

    Добавьте файлы проекта.

---
+++ Task4/Task4Doc.cpp
@@ -0,0 +1,138 @@
+﻿
+// Task4Doc.cpp: реализация класса CTask4Doc 
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS можно определить в обработчиках фильтров просмотра реализации проекта ATL, эскизов
+// и поиска; позволяет совместно использовать код документа в данным проекте.
+#ifndef SHARED_HANDLERS
+#include "Task4.h"
+#endif
+
+#include "Task4Doc.h"
+
+#include <propkey.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+// CTask4Doc
+
+IMPLEMENT_DYNCREATE(CTask4Doc, CDocument)
+
+BEGIN_MESSAGE_MAP(CTask4Doc, CDocument)
+END_MESSAGE_MAP()
+
+
+// Создание или уничтожение CTask4Doc
+
+CTask4Doc::CTask4Doc() noexcept
+{
+	// TODO: добавьте код для одноразового вызова конструктора
+
+}
+
+CTask4Doc::~CTask4Doc()
+{
+}
+
+BOOL CTask4Doc::OnNewDocument()
+{
+	if (!CDocument::OnNewDocument())
+		return FALSE;
+
+	// TODO: добавьте код повторной инициализации
+	// (Документы SDI будут повторно использовать этот документ)
+
+	return TRUE;
+}
+
+
+
+
+// Сериализация CTask4Doc
+
+void CTask4Doc::Serialize(CArchive& ar)
+{
+	if (ar.IsStoring())
+	{
+		// TODO: добавьте код сохранения
+	}
+	else
+	{
+		// TODO: добавьте код загрузки
+	}
+}
+
+#ifdef SHARED_HANDLERS
+
+// Поддержка для эскизов
+void CTask4Doc::OnDrawThumbnail(CDC& dc, LPRECT lprcBounds)
+{
+	// Измените этот код для отображения данных документа
+	dc.FillSolidRect(lprcBounds, RGB(255, 255, 255));
+
+	CString strText = _T("TODO: implement thumbnail drawing here");
+	LOGFONT lf;
+
+	CFont* pDefaultGUIFont = CFont::FromHandle((HFONT) GetStockObject(DEFAULT_GUI_FONT));
+	pDefaultGUIFont->GetLogFont(&lf);
+	lf.lfHeight = 36;
+
+	CFont fontDraw;
+	fontDraw.CreateFontIndirect(&lf);
+
+	CFont* pOldFont = dc.SelectObject(&fontDraw);
+	dc.DrawText(strText, lprcBounds, DT_CENTER | DT_WORDBREAK);
+	dc.SelectObject(pOldFont);
+}
+
+// Поддержка обработчиков поиска
+void CTask4Doc::InitializeSearchContent()
+{
+	CString strSearchContent;
+	// Задание содержимого поиска из данных документа.
+	// Части содержимого должны разделяться точкой с запятой ";"
+
+	// Например:  strSearchContent = _T("точка;прямоугольник;круг;объект ole;");
+	SetSearchContent(strSearchContent);
+}
+
+void CTask4Doc::SetSearchContent(const CString& value)
+{
+	if (value.IsEmpty())
+	{
+		RemoveChunk(PKEY_Search_Contents.fmtid, PKEY_Search_Contents.pid);
+	}
+	else
+	{
+		CMFCFilterChunkValueImpl *pChunk = nullptr;
+		ATLTRY(pChunk = new CMFCFilterChunkValueImpl);
+		if (pChunk != nullptr)
+		{
+			pChunk->SetTextValue(PKEY_Search_Contents, value, CHUNK_TEXT);
+			SetChunkValue(pChunk);
+		}
+	}
+}
+
+#endif // SHARED_HANDLERS
+
+// Диагностика CTask4Doc
+
+#ifdef _DEBUG
+void CTask4Doc::AssertValid() const
+{
+	CDocument::AssertValid();
+}
+
+void CTask4Doc::Dump(CDumpContext& dc) const
+{
+	CDocument::Dump(dc);
+}
+#endif //_DEBUG
+
+
+// Команды CTask4Doc
